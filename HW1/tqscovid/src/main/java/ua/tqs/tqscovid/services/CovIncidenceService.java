22248fe108d332f9d1f0cb15705a40be99e74126
VascoRegal <vascoregal24@gmail.com>
2022-04-09 16:21:54 +0100

    service

--- HW1/tqscovid/src/main/java/ua/tqs/tqscovid/services/CovIncidenceService.java
+++ HW1/tqscovid/src/main/java/ua/tqs/tqscovid/services/CovIncidenceService.java
@@ -14,21 +14,34 @@ import java.util.List;
 
 @Service
 public class CovIncidenceService {
+    
+    private IExternalAPIAdapter externalAPIAdapter;
+
+    public CovIncidenceService(IExternalAPIAdapter externalAPIAdapter) {
+        this.externalAPIAdapter = externalAPIAdapter;
+    }
 
     public List<DailyStats> getAllStats() throws ParseException, IOException, URISyntaxException {
-        return null;
+        return this.externalAPIAdapter.getAllStats();
     }
 
     public List<Country> getCountries() throws URISyntaxException, ParseException, IOException {
-        return null;
+        return this.externalAPIAdapter.getCountries();
 
     };
     public List<DailyStats> getStatsByCountry(String country) throws ParseException, IOException, URISyntaxException {
-        return null;
+        return this.externalAPIAdapter.getStatsByCountry(country);
     };
 
     public DailyStats getStatsByDay(String date, String country) throws URISyntaxException, ParseException, IOException {
-        return null;
+        LocalDate ldate;
+        try {
+            ldate = LocalDate.parse(date);
+        } catch(Exception e) {
+            return new DailyStats(null, null, null, null, null);
+        }
+
+        return this.externalAPIAdapter.getStatsByDay(ldate, country);
     };
  
 }
