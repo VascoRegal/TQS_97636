9e583dd9844ab28e5e7530ae3907ba9e3a1dc291
VascoRegal <vascoregal24@gmail.com>
2022-04-19 17:50:43 +0100

    update dockers

--- HW1/prod.sh
+++ HW1/prod.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-cd tqscovid && mvn clean package
-cd ../
-sudo docker-compose down -v
-sudo docker-compose build
-sudo docker-compose up -d
+docker-compose down -v
+docker-compose build
+docker-compose up -d
