aa731385eda4eb84190c88c611cebc22cb523afa
VascoRegal <vascoregal24@gmail.com>
2022-04-22 17:10:25 +0100

    util testing

--- HW1/tqscovid/src/test/java/ua/tqs/tqscovid/api/HttpClientImplTest.java
+++ HW1/tqscovid/src/test/java/ua/tqs/tqscovid/api/HttpClientImplTest.java
@@ -1,10 +1,18 @@
 package ua.tqs.tqscovid.api;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ua.tqs.tqscovid.http.CovIncidenceHttpClient;
 import ua.tqs.tqscovid.http.IHttpClient;
+import ua.tqs.tqscovid.utils.JsonUtils;
 
 public class HttpClientImplTest {
     
@@ -16,7 +24,14 @@ public class HttpClientImplTest {
     }
 
     @Test
-    void doGet_RightResults() {
-        
+    void doGet_RightResultsAndReturnsJSON() throws IOException, ParseException {
+        String uri = "https://jsonplaceholder.typicode.com/todos/1";
+
+        String get = this.httpClient.doGet(uri, new HashMap<>());
+
+        JSONObject obj = JsonUtils.responseToJson(get);
+        System.out.println(obj.get("userId").getClass());
+        assertTrue(obj.get("userId").equals(1L));
+        assertTrue(obj.get("title").equals("delectus aut autem"));
     }
 }
